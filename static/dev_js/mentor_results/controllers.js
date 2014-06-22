62056d6393f0c40fe10886b753fdc62052a45e0b
FriedrichK <friedrich@cartogami.com>
2014-06-22 15:43:51 +0200

    refactored to remove unnecessary controller

--- static/dev_js/mentor_results/controllers.js
+++ static/dev_js/mentor_results/controllers.js
@@ -1,20 +1,14 @@
 /* global angular, document */
 
 var app = angular.module('app'); 
-app.controller('RefineSearchController', function($scope, mentorSearchService, mentorSettingsService, refineSettingsService) {
-    $scope.refine = mentorSettingsService.initialSettings;
+app.controller('RefineSearchController', function($scope, $element, mentorSearchService, mentorSettingsService, refineSettingsService) {
+    angular.element($element).ready(function() {
+        $scope.refine = refineSettingsService.get();
+    });
     $scope.change = function(evt) {
-        mentorSearchService.getMatchingMentors($scope.refine);
+        mentorSearchService.getMatchingMentors($scope.refine, function() {});
         refineSettingsService.set($scope.refine);
     };
-    $scope.test = function(evt) {
-    };
-});
-
-app.controller('DocumentReadyController', function(refineSettingsService) {
-    angular.element(document).ready(function() {
-        var state = refineSettingsService.get();
-    });
 });
 
 app.controller('MentorListController', function($scope, mentorSearchService) {
