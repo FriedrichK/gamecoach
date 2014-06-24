4c98da01330a2e2bfbd7d09039c2dddc09b3673e
FriedrichK <friedrich@cartogami.com>
2014-06-24 18:53:26 +0200

    automatic static update

--- static/js/mentor_profile.js
+++ static/js/mentor_profile.js
@@ -30,6 +30,35 @@ app.directive('valueAsPercent', function() {
         }
     };
 });
+
+app.directive('responseTimeAsText', function() {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        template: '<a>{{responseTimeAsText}}</a>',
+        link: function(scope, element, attrs, ngModel) {
+            scope.$watch(attrs.responseTimeAsText, function(newValue) {
+                if(scope.profile && scope.profile.data && scope.profile.data.response_time) {
+                    var value = scope.profile.data.response_time;
+                    var text = "More than one day";
+                    if(value < 7) {
+                        text = value + " days";
+                    }
+                    if(value < 1) {
+                        text = "Less than a day";
+                    }
+                    if(value === 7) {
+                        text = "One week";
+                    }
+                    if(value > 7) {
+                        text = "More than a week";
+                    }
+                    scope.responseTimeAsText = text;
+                }
+            });
+        }
+    };
+});
 /* global angular */
 
 var app = angular.module('app'); 
